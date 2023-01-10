e6715e1ee7a06b7375c6c74acdc9fb1c09764439
pmorelr <pmorelrosa@gmail.com>
2023-01-10 16:39:26 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='ENSTA Paris student project in partnership with BNP Paribas focused on document layout analysis and segmentation, using state-of-the-art AI tools.',
+    author='Your name (or your organization/company/team)',
+    license='MIT',
+)
