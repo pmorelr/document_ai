e4d238b4cb9a48b3365db41f61f169a3dadeceae
pmorelr <pmorelrosa@gmail.com>
2023-03-16 13:13:28 +0100

    Corrected bug for "merged" approach

--- src/features/noise_management.py
+++ src/features/noise_management.py
@@ -4,7 +4,7 @@ def noise_management(dataset, noise_manag):
     if noise_manag == "all":
         return dataset
     elif noise_manag == "merged":
-        dataset = dataset.map(lambda x: {'tags': [y if y != "Page-Footer" and y != "Page-Header" else "Noise" for y in x['tags']]})
+        dataset = dataset.map(lambda x: {'tags': [0 if y not in [1,4,5] else 1 for y in x['tags']]})
         return dataset
     elif noise_manag == "ignored":
         dataset = dataset.filter(lambda x: "Page-Footer" not in x['tags'] and "Page-Header" not in x['tags'])
