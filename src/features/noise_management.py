f4485fc06dbdfa1e36eda441a4c7e36a4e794745
pmorelr <pmorelrosa@gmail.com>
2023-03-22 13:28:14 +0100

    Added triplet appoach

--- src/features/noise_management.py
+++ src/features/noise_management.py
@@ -3,11 +3,12 @@
 def noise_management(dataset, noise_manag):
     if noise_manag == "all":
         return dataset
-    elif noise_manag == "merged":
+    elif noise_manag == "binary":
         dataset = dataset.map(lambda x: {'tags': [0 if y not in [1,4,5] else 1 for y in x['tags']]})
         return dataset
-    elif noise_manag == "ignored":
-        dataset = dataset.filter(lambda x: "Page-Footer" not in x['tags'] and "Page-Header" not in x['tags'])
+    elif noise_manag == "triplet":
+        reclass = [0,1,0,0,2,3]
+        dataset = dataset.map(lambda x: {'tags': [0 if y not in [1,4,5] else reclass[y] for y in x['tags']]})
         return dataset
     else:
-        raise ValueError("Invalid noise management option. Please choose between 'all', 'merged' and 'ignored'")
+        raise ValueError("Invalid noise management option. Please choose between 'all', 'binary' and 'triplet'")
