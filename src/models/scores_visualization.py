bb59baade8fc691703aec28c93904f4a93a6dd31
pmorelr <pmorelrosa@gmail.com>
2023-03-24 19:38:10 +0100

    Added visualization script

--- src/models/scores_visualization.py
+++ src/models/scores_visualization.py
@@ -1,10 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
-
-
-s = {'Caption': {'precision': 0.42857142857142855, 'recall': 0.42857142857142855, 'f1': 0.42857142857142855, 'number': 7}, 'Footnote': {'precision': 1.0, 'recall': 1.0, 'f1': 1.0, 'number': 4}, 'Page-Footer': {'precision': 1.0, 'recall': 0.9655172413793104, 'f1': 0.9824561403508771, 'number': 29}, 'Page-Header': {'precision': 1.0, 'recall': 0.9090909090909091, 'f1': 0.9523809523809523, 'number': 33}, 'Picture': {'precision': 0.3956639566395664, 'recall': 0.7192118226600985, 'f1': 0.5104895104895105, 'number': 203}, 'Table': {'precision': 0.9581005586592178, 'recall': 0.9985443959243085, 'f1': 0.9779044903777621, 'number': 1374}, 'Text': {'precision': 0.9930615784908933, 'recall': 0.8339402767662054, 'f1': 0.9065716547901822, 'number': 1373}, 'Title': {'precision': 1.0, 'recall': 1.0, 'f1': 1.0, 'number': 1}, 'overall_precision': 0.90244708994709, 'overall_recall': 0.90244708994709, 'overall_f1': 0.90244708994709, 'overall_accuracy': 0.90244708994709}
-
+sns.set()
 
 # Retrieve the scores and plot them
 def plot_scores(scores):
@@ -16,22 +13,23 @@ def plot_scores(scores):
     labels = [label for label in scores.keys() if label != 'overall_precision' and label != 'overall_recall' and label != 'overall_f1' and label != 'overall_accuracy']
     
     # Set the figure size
-    plt.figure(figsize=(20, 10))
+    plt.figure(figsize=(22, 10))
+
+    plt.rcParams['text.usetex'] = True
+    palette = sns.color_palette().as_hex()
     
     # Plot the scores
     plt.subplot(2, 2, 1)
-    plt.title("Precision")
-    plt.bar(labels, precisions)
+    plt.title(r'\textbf{Precision}', fontsize = 20)
+    plt.bar(labels, precisions, color=palette, width=0.6)
     plt.subplot(2, 2, 2)
-    plt.title("Recall")
-    plt.bar(labels, recalls)
+    plt.title(r'\textbf{Recall}', fontsize = 20)
+    plt.bar(labels, recalls, color=palette, width=0.6)
     plt.subplot(2, 2, 3)
-    plt.title("F1")
-    plt.bar(labels, f1s)
+    plt.title(r'\textbf{f1 Score}', fontsize = 20)
+    plt.bar(labels, f1s, color=palette, width=0.6)
     plt.subplot(2, 2, 4)
-    plt.title("Number of elements")
-    plt.bar(labels, accuracies)
+    plt.title(r'\textbf{Number of Elements}', fontsize = 20)
+    plt.bar(labels, accuracies, color=palette, width=0.6)
     
     plt.show()
-
-plot_scores(s)
